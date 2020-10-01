56ec52af4347246dc37aacc07848657457816655
dzou <dzou@users.noreply.github.com>
2020-10-01 16:01:45 -0400

    increase ram

--- .cloudbuild/graal-build-script.sh
+++ .cloudbuild/graal-build-script.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 gu install native-image
-echo ${JAVA_HOME}
-java --version
 ./mvnw clean install -B -q
 ./mvnw package -P graal -B --file google-cloud-graalvm-samples/
 ./google-cloud-graalvm-samples/google-cloud-graalvm-pubsub-sample/target/com.example.pubsubsampleapplication
