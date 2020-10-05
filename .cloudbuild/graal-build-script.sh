d0184139ed0041abb1ba8cbca661c80295ce723e
dzou <dzou@users.noreply.github.com>
2020-10-05 17:57:38 -0400

    fix sample names

--- .cloudbuild/graal-build-script.sh
+++ .cloudbuild/graal-build-script.sh
@@ -3,4 +3,4 @@
 gu install native-image
 ./mvnw clean install -B -q
 ./mvnw package -P graal -B --file google-cloud-graalvm-samples/
-./google-cloud-graalvm-samples/google-cloud-graalvm-pubsub-sample/target/com.example.pubsubsampleapplication
+./google-cloud-graalvm-samples/pubsub-sample/target/com.example.pubsubsampleapplication
