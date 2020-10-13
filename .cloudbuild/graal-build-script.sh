d355d755b1a208705af7ea9ce436dfa487589eaf
dzou <dzou@users.noreply.github.com>
2020-10-12 20:49:03 -0400

    tmp build change to test

--- .cloudbuild/graal-build-script.sh
+++ .cloudbuild/graal-build-script.sh
@@ -2,4 +2,5 @@
 
 gu install native-image
 ./mvnw clean install -B -q
-./mvnw package -P graal -B --file google-cloud-graalvm-samples
+./mvnw package -P graal -B --file google-cloud-graalvm-samples/pubsub-sample
+./mvnw package -P graal -B --file google-cloud-graalvm-samples/storage-sample
