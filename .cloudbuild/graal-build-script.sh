a5ba42bcc5352c67bf154ef3f82d1ac86a41424c
dzou <dzou@users.noreply.github.com>
2020-10-26 16:36:56 -0400

    try to fix build

--- .cloudbuild/graal-build-script.sh
+++ .cloudbuild/graal-build-script.sh
@@ -2,4 +2,4 @@
 
 gu install native-image
 ./mvnw clean install -B -q
-./mvnw package -P graal -B --file google-cloud-graalvm-samples
+./mvnw package -P graal -B --file google-cloud-graalvm-samples -Dquarkus.native.container-build=true
