3a84bdf19b35fa5779fb63aecff2f0d3bda8ebb9
dzou <dzou@users.noreply.github.com>
2021-03-30 16:41:47 -0400

    Make the ProtobufMessageFeature optional.

--- google-cloud-graalvm-support/src/main/java/com/google/cloud/graalvm/features/ProtobufMessageFeature.java
+++ google-cloud-graalvm-support/src/main/java/com/google/cloud/graalvm/features/ProtobufMessageFeature.java
@@ -14,26 +14,39 @@
  * limitations under the License.
  */
 
-package com.google.cloud.graalvm.features.core;
+package com.google.cloud.graalvm.features;
 
 import static com.google.cloud.graalvm.features.NativeImageUtils.getMethodOrFail;
 import static com.google.cloud.graalvm.features.NativeImageUtils.registerClassHierarchyForReflection;
 
-import com.oracle.svm.core.annotate.AutomaticFeature;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
 /**
- * A feature which registers reflective usages of the GRPC Protobuf libraries.
+ * A optional feature which registers reflective usages of the GRPC Protobuf libraries.
+ *
+ * <p>This feature is only needed if you need to access proto objects reflectively
+ * (such as printing/logging proto objects).
+ *
+ * <p>To add this feature, add
+ * "--feature com.google.cloud.graalvm.features.ProtobufMessageFeature"
+ * to your GraalVM configuration.
  */
-@AutomaticFeature
 public class ProtobufMessageFeature implements Feature {
 
+  // Proto classes to check on the classpath.
   private static final String PROTO_MESSAGE_CLASS = "com.google.protobuf.GeneratedMessageV3";
   private static final String PROTO_ENUM_CLASS = "com.google.protobuf.ProtocolMessageEnum";
   private static final String ENUM_VAL_DESCRIPTOR_CLASS =
       "com.google.protobuf.Descriptors$EnumValueDescriptor";
+
+  // Prefixes of methods accessed reflectively by
+  // com.google.protobuf.GeneratedMessageV3$ReflectionInvoker
+  private static final List<String> METHOD_ACCESSOR_PREFIXES =
+      Arrays.asList("get", "set", "has", "add", "clear", "newBuilder");
 
   @Override
   public void beforeAnalysis(BeforeAnalysisAccess access) {
@@ -45,12 +58,13 @@ public class ProtobufMessageFeature implements Feature {
       Method internalAccessorMethod =
           getMethodOrFail(protoMessageClass, "internalGetFieldAccessorTable");
 
-      // Finds every class whose `internalGetFieldAccessorTable()` and registers the class.
+      // Finds every class whose `internalGetFieldAccessorTable()` is reached and registers it.
       // `internalGetFieldAccessorTable()` is used downstream to access the class reflectively.
       access.registerMethodOverrideReachabilityHandler(
-          (duringAccess, method) ->
-            registerClassHierarchyForReflection(
-                duringAccess, method.getDeclaringClass().getName()),
+          (duringAccess, method) -> {
+            registerFieldAccessors(method.getDeclaringClass());
+            registerFieldAccessors(getBuilderClass(method.getDeclaringClass()));
+          },
           internalAccessorMethod);
     }
 
@@ -72,5 +86,30 @@ public class ProtobufMessageFeature implements Feature {
           },
           protoEnumClass);
     }
+  }
+
+  /**
+   * Given a proto class, registers the public accessor methods for the provided proto class.
+   */
+  private static void registerFieldAccessors(Class<?> protoClass) {
+    for (Method method : protoClass.getMethods()) {
+      boolean hasAccessorPrefix =
+          METHOD_ACCESSOR_PREFIXES.stream().anyMatch(prefix -> method.getName().startsWith(prefix));
+      if (hasAccessorPrefix) {
+        RuntimeReflection.register(method);
+      }
+    }
+  }
+
+  /**
+   * Given a proto class, returns the Builder nested class.
+   */
+  private static Class<?> getBuilderClass(Class<?> protoClass) {
+    for (Class<?> clazz : protoClass.getClasses()) {
+      if (clazz.getName().endsWith("Builder")) {
+        return clazz;
+      }
+    }
+    return null;
   }
 }
