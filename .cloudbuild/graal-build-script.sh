c2f2774f71157f554c2e349237cee37152881ea6
dzou <dzou@users.noreply.github.com>
2021-03-30 16:49:23 -0400

    experiment with parallelization settings

--- .cloudbuild/graal-build-script.sh
+++ .cloudbuild/graal-build-script.sh
@@ -2,4 +2,4 @@
 
 gu install native-image
 ./mvnw clean install -B -q
-./mvnw verify -P graal -B --file google-cloud-graalvm-samples
+./mvnw verify -T 1.5C -P graal -B --file google-cloud-graalvm-samples
