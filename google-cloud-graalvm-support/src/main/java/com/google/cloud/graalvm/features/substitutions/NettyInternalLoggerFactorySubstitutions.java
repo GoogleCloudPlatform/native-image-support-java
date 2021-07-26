617a5e897d6791ca77b608db9a46f5f4eb660825
dzou <dzou@users.noreply.github.com>
2021-07-26 11:27:48 -0400

    fix substitutions and delete extra configs

--- google-cloud-graalvm-support/src/main/java/com/google/cloud/graalvm/features/substitutions/NettyInternalLoggerFactorySubstitutions.java
+++ google-cloud-graalvm-support/src/main/java/com/google/cloud/graalvm/features/substitutions/NettyInternalLoggerFactorySubstitutions.java
@@ -20,18 +20,37 @@ import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 import io.grpc.netty.shaded.io.netty.util.internal.logging.InternalLoggerFactory;
 import io.grpc.netty.shaded.io.netty.util.internal.logging.JdkLoggerFactory;
+import java.util.function.BooleanSupplier;
 
 /**
  * Substitutions for {@link InternalLoggerFactory} which are needed to avoid dynamic loading
  * of logging library.
  */
 @TargetClass(
-    className = "io.grpc.netty.shaded.io.netty.util.internal.logging.InternalLoggerFactory"
+    className = "io.grpc.netty.shaded.io.netty.util.internal.logging.InternalLoggerFactory",
+    onlyWith = NettyInternalLoggerFactorySubstitutions.OnlyIfInClassPath.class
 )
 final class NettyInternalLoggerFactorySubstitutions {
 
   @Substitute
   private static InternalLoggerFactory newDefaultFactory(String name) {
     return JdkLoggerFactory.INSTANCE;
+  }
+
+  static class OnlyIfInClassPath implements BooleanSupplier {
+
+    @Override
+    public boolean getAsBoolean() {
+      try {
+        // Note: Set initialize = false to avoid initializing the class when looking it up.
+        Class.forName(
+            "io.grpc.netty.shaded.io.netty.util.internal.logging.InternalLoggerFactory",
+            false,
+            Thread.currentThread().getContextClassLoader());
+        return true;
+      } catch (ClassNotFoundException e) {
+        return false;
+      }
+    }
   }
 }
